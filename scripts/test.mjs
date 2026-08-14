b12f5a859261203d4aa9780f49a49c688597e61e
track507 <trackbeforedawn@gmail.com>
2026-08-14 09:11:01 -0500

    chore: Removed a test script

--- scripts/test.mjs
+++
@@ -1,8 +0,0 @@
-console.log(Temporal.Now.plainDateTimeISO().toString());
-console.log(new Date().toISOString().replace("T", " ").slice(0, 19));
-console.log(Intl.DateTimeFormat().resolvedOptions().timeZone);
-console.log(new Date().getTimezoneOffset());
-console.log(new Date().toString());
-console.log(process.env.TZ);
-console.log(Temporal.Now.instant().toString());
-console.log(Temporal.Now.instant().toString().replace("T", " ").slice(0, 19));
